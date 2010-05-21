a090e25732b31315bfa95c80f7fda0bb86462c02
Squeeks <privacymyass@gmail.com>
2010-05-21 17:18:30 +0100

    Gearing up for v0.04.

--- lib/WWW/Pusher.pm
+++ lib/WWW/Pusher.pm
@@ -22,11 +22,11 @@ WWW::Pusher - Interface to the Pusher WebSockets API
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
