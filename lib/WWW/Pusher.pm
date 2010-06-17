a2488bc8331e8242e4781992daa970079d517537
Squeeks <squeek@cpan.org>
2010-06-17 19:37:26 +0100

    Version Bump to 0.05.

--- lib/WWW/Pusher.pm
+++ lib/WWW/Pusher.pm
@@ -22,11 +22,11 @@ WWW::Pusher - Interface to the Pusher WebSockets API
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 =head1 SYNOPSIS
 
