46d9cd0274ff52ece72709ed6b7927d1f4a442a7
Squeeks <privacymyass@gmail.com>
2010-08-19 17:39:38 +0100

    Changelog and version to 0.06.

--- lib/WWW/Pusher.pm
+++ lib/WWW/Pusher.pm
@@ -22,11 +22,11 @@ WWW::Pusher - Interface to the Pusher WebSockets API
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
